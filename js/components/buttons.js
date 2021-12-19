c2215b92f2b6dcb2ed12e6c180b0a1950ec58919
Jennygra <83270722+Jennygra@users.noreply.github.com>
2021-12-19 09:27:38 +0100

    Added add to bag function

--- js/components/buttons.js
+++ js/components/buttons.js
@@ -2,9 +2,43 @@
 //Product page 
 const addToCartBtn = document.querySelector("#addToCart");  
 
-addToCartBtn.onclick = function() {
-    addToCartBtn.innerHTML = `Added to bag`; 
+const colorChoicesContainer = document.querySelector("#colorSelect"); 
+const sizeChoicesContainer = document.querySelector("#sizeSelect");
+
+const colorError = document.querySelector("#colorError"); 
+const sizeError = document.querySelector("#sizeError");
+
+function productSelection() {
+    event.preventDefault(); 
+
+    let isSelected = true; 
+
+    if(colorChoicesContainer.value > 0) {
+        colorError.style.display = "none"; 
+    } else {
+        colorError.style.display = "block"; 
+        return false
+    }
+
+    if(sizeChoicesContainer.value > 0) {
+        sizeError.style.display = "none"; 
+    } else {
+        sizeError.style.display = "block"; 
+        return false; 
+    }
+
+    return isSelected; 
 }
+
+function addToCart() {
+    if(!productSelection()) {
+        return false; 
+    } else {
+        addToCartBtn.innerHTML = `Added to bag`; 
+    }
+}
+
+
 
 // //Collection page 
 // const showMoreCollection = document.querySelector("#showMoreCollection"); 
