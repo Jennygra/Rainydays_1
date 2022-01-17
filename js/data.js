1e829b77c62c1abff7511300212f88dd2a569e4c
Jennygra <83270722+Jennygra@users.noreply.github.com>
2022-01-17 19:36:23 +0100

    Wordpress update

--- js/data.js
+++ js/data.js
@@ -57,28 +57,28 @@ const productData = [
     }, 
 ]; 
 
-const featureProductContainer = document.querySelector(".homepage-feature-collection-container"); 
+// const featureProductContainer = document.querySelector(".homepage-feature-collection-container"); 
 
 
-for(let i = 0; i < productData.length; i++) {
-    const name = productData[i].name; 
-    const price = productData[i].cost;
-    const sex = productData[i].sex;
-    const productImg = productData[i].image;
+// for(let i = 0; i < productData.length; i++) {
+//     const name = productData[i].name; 
+//     const price = productData[i].cost;
+//     const sex = productData[i].sex;
+//     const productImg = productData[i].image;
 
-    featureProductContainer.innerHTML += `
-    <div class="feature-collection">
-    <a href="product.html"><img src="images/collection-page/${productImg}" alt="product image of the Blue Jacket"></a>
+//     featureProductContainer.innerHTML += `
+//     <div class="feature-collection">
+//     <a href="product.html"><img src="images/collection-page/${productImg}" alt="product image of the Blue Jacket"></a>
 
-    <div>
-    <p>${sex}<p>
-    <h4>${name}<h4>
-    <p>Nok ${price}<p>
-    </div>
+//     <div>
+//     <p>${sex}<p>
+//     <h4>${name}<h4>
+//     <p>Nok ${price}<p>
+//     </div>
 
-    <div class="likeCta">
-    <i onclick="liked(this)" class="far fa-heart" id="likeIcon"></i>
-    </div>
-    </div>
-    `;
-}
+//     <div class="likeCta">
+//     <i onclick="liked(this)" class="far fa-heart" id="likeIcon"></i>
+//     </div>
+//     </div>
+//     `;
+// }
---
+++ js/script.js
@@ -0,0 +1,37 @@
+const url = "http://localhost:8888/wp-json/wc/store/products"; 
+const featureProductContainer = document.querySelector(".homepage-feature-collection-container"); 
+
+async function getProducts(){
+    try{
+        const response = await fetch(url); 
+        const getResults = await response.json(); 
+        createHTML(getResults);
+    } 
+
+    catch(error) {
+        console.log("This is an error");
+    }
+}
+
+getProducts();
+
+function createHTML(products){
+    products.forEach(function(product){
+        featureProductContainer.innerHTML += `
+            <div class="feature-collection">
+            <img src="${product.images[0].src}" alt="${product.images[0].alt}">
+        
+            <div>
+            <p>woman/men<p>
+            <h4>${product.name}<h4>
+            <p>Nok ${product.prices.price}<p>
+            </div>
+        
+            <div class="likeCta">
+            <i onclick="liked(this)" class="far fa-heart" id="likeIcon"></i>
+            </div>
+            </div>
+            `;
+    });
+}
+
